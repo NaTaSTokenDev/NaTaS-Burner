dc269b5f7754ee2c92be4f86eb5f3550dcb767ba
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-10-17 13:38:41 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -125,6 +125,16 @@ const App = () => {
                 <h3 className="text-align-center">
                   Current counter: <span>{storage}</span>
                 </h3>
+                <div class="grid-container">
+                  <div>1</div>
+                  <div>2</div>
+                  <div>3</div>  
+                  <div>4</div>
+                  <div>5</div>
+                  <div>6</div>
+                  <div>7</div>
+                  <div>8</div>
+                </div>
                 <UpdateContract
                   contract={contract}
                   setUserBalance={setUserBalance}
