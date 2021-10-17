298f3ea66069aefdf007f66e965ef0cca6532eab
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-10-17 14:50:47 -0400

    Update Transfers.tsx

--- src/components/Transfers.tsx
+++ src/components/Transfers.tsx
@@ -36,21 +36,21 @@ const Transfers = ({
 
   return (
     <div id="transfer-inputs">
-//      <input
- //      type="text"
- //      placeholder="Recipient"
- //       value={recipient}
-  //      onChange={e => setRecipient(e.target.value)}
+{/*       <input
+       type="text"
+       placeholder="Recipient"
+        value={recipient}
+        onChange={e => setRecipient(e.target.value)}    */} 
       />
-  //    <input
- //       type="number"
- //       placeholder="Amount"
-  //      value={amount}
- //       onChange={e => setAmount(e.target.value)}
+ {/*     <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={e => setAmount(e.target.value)}   */} 
       />
       <button
         className="button"
-//        disabled={!recipient && !amount}
+    {/* disabled={!recipient && !amount}     */} 
         onClick={sendTransfer}
       >
         {loading ? (
