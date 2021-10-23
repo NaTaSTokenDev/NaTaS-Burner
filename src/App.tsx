db8f83756aea4b1e8f68393126fa5909f507b11d
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-10-23 08:47:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -178,11 +178,11 @@ const App = () => {
             )}
             <p>
               <i className="far fa-file-code"></i>&nbsp;
- //             <a
-  //              href={`https://better-call.dev/granadanet/${contractAddress}/operations`}
-  //              target="_blank"
-   //             rel="noopener noreferrer"
-  //            >
+              <a
+                href={`https://better-call.dev/granadanet/${contractAddress}/operations`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {contractAddress}
               </a>
             </p>
