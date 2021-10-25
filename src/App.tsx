2c698559e024d2d6ec812d292f77068f54f5856a
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-10-24 21:53:20 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -167,6 +167,66 @@ const App = () => {
                   <div>38</div>
                   <div>39</div>
                   <div>40</div>
+                  <div>41</div>
+                  <div>42</div>
+                  <div>43</div>  
+                  <div>44</div>
+                  <div>45</div>
+                  <div>46</div>
+                  <div>47</div>
+                  <div>48</div>
+                  <div>49</div>
+                  <div>50</div>
+                  <div>51</div>
+                  <div>52</div>
+                  <div>53</div>  
+                  <div>54</div>
+                  <div>55</div>
+                  <div>56</div>
+                  <div>57</div>
+                  <div>58</div>
+                  <div>59</div>
+                  <div>60</div>
+                  <div>61</div>
+                  <div>62</div>
+                  <div>63</div>  
+                  <div>64</div>
+                  <div>65</div>
+                  <div>66</div>
+                  <div>67</div>
+                  <div>68</div>
+                  <div>69</div>
+                  <div>70</div>
+                  <div>71</div>
+                  <div>72</div>
+                  <div>73</div>  
+                  <div>74</div>
+                  <div>75</div>
+                  <div>76</div>
+                  <div>77</div>
+                  <div>78</div>
+                  <div>79</div>
+                  <div>80</div>
+                  <div>81</div>
+                  <div>82</div>
+                  <div>83</div>  
+                  <div>84</div>
+                  <div>85</div>
+                  <div>86</div>
+                  <div>87</div>
+                  <div>88</div>
+                  <div>89</div>
+                  <div>90</div>
+                  <div>91</div>
+                  <div>92</div>
+                  <div>93</div>  
+                  <div>94</div>
+                  <div>95</div>
+                  <div>96</div>
+                  <div>97</div>
+                  <div>98</div>
+                  <div>99</div>
+                  <div>100</div>
                 </div>
                 <UpdateContract
                   contract={contract}
