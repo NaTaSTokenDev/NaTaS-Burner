0749ed2d9784312da4baa91b05f5cdbae16b2a8a
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-07 09:01:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
   const [Tezos, setTezos] = useState<TezosToolkit>(
     new TezosToolkit("https://mainnet.api.tez.ie")
   );
-  const [contract, setContract] = useState<any>(undefined);
+ // const [contract, setContract] = useState<any>(undefined);
   const [publicToken, setPublicToken] = useState<string | null>("");
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
@@ -29,7 +29,8 @@ const App = () => {
   const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
   const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState<string>("transfer");
-
+  const contract = await Tezos.contract.at(contractAddress);
+  
   // Granadanet Increment/Decrement contract
  // const contractAddress: string = "KT1K3XVNzsmur7VRgY8CAHPUENaErzzEpe4e";
   
@@ -267,7 +268,7 @@ const App = () => {
              <p className="text-align-center">
                {/*         <i className="fas fa-piggy-bank"></i>&nbsp;   */}
                 Balance {(userBalance / 1000000).toLocaleString("en-US")} DeMN Tokens
-               Balance {(storage2 / 1000000).toLocaleString("en-US")} DeMN Tokens
+               Balance {(storage / 1000000).toLocaleString("en-US")} DeMN Tokens
             </p>
           </div>
           <DisconnectButton
