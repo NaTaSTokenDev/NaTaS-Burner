eae2cc18a72c823e404a8412b2740aed2625d654
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-07 09:08:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
   const [Tezos, setTezos] = useState<TezosToolkit>(
     new TezosToolkit("https://mainnet.api.tez.ie")
   );
- // const [contract, setContract] = useState<any>(undefined);
+  const [contract, setContract] = useState<any>(undefined);
   const [publicToken, setPublicToken] = useState<string | null>("");
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
@@ -29,7 +29,7 @@ const App = () => {
   const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
   const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState<string>("transfer");
-  const contract = await Tezos.contract.at(contractAddress);
+  // const contract = await Tezos.contract.at(contractAddress);
   
   // Granadanet Increment/Decrement contract
  // const contractAddress: string = "KT1K3XVNzsmur7VRgY8CAHPUENaErzzEpe4e";
