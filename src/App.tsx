675580e65dbf3a1c87646b3f0312fa323155c522
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-07 07:55:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,25 +20,25 @@ const App = () => {
   const [Tezos, setTezos] = useState<TezosToolkit>(
     new TezosToolkit("https://mainnet.api.tez.ie")
   );
-  const [contract, setContract] = useState<any>(undefined);
+  // const [contract, setContract] = useState<any>(undefined);
   const [publicToken, setPublicToken] = useState<string | null>("");
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
   const [userBalance, setUserBalance] = useState<number>(0);
-  const [storage, setStorage] = useState<number>(0);
+//  const [storage, setStorage] = useState<number>(0);
   const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
   const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState<string>("transfer");
 
   // Granadanet Increment/Decrement contract
-  const contractAddress: string = "KT1K3XVNzsmur7VRgY8CAHPUENaErzzEpe4e";
+ // const contractAddress: string = "KT1K3XVNzsmur7VRgY8CAHPUENaErzzEpe4e";
   
   // DeMN Contract Address
-  const contractAddress2: string = "KT1GBgCd5dk7v4TSzWvtk1X64TxMyG4r7eRX";
+  const contractAddress: string = "KT1GBgCd5dk7v4TSzWvtk1X64TxMyG4r7eRX";
 
-  const contract = await Tezos.contract.at(contractAddress2);
-  const storage2: MichelsonMap<string, BigNumber> = await
-  contract.storage2();
+  const contract = await Tezos.contract.at(contractAddress);
+  const storage: MichelsonMap<string, BigNumber> = await
+  contract.storage();
   
   const generateQrCode = (): { __html: string } => {
     const qr = qrcode(0, "L");
