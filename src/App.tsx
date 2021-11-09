735d72b1776f11fedcd1ee3e5b00c5a198ea23aa
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-08 19:13:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,8 @@ const App = () => {
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
   const [userBalance, setUserBalance] = useState<number>(0);
-  const [storage, setStorage] = useState<number>(666); 
-  const [storage2, setStorage2] = useState<number>(446);
+  const [storage, setStorage] = useState<number>(0); 
+  const [storage2, setStorage2] = useState<number>(0);
   const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
   const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState<string>("transfer");
