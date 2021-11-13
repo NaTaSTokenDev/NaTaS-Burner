714d61d7f14c77975de41c06fa114db4f490d877
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-12 19:35:46 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ enum BeaconConnection {
 }
 
 const App = () => {
-  const [Tezos, setTezos] = useState<TezosToolkit> (new TezosToolkit("https://mainnet.api.tez.ie");
+  const [Tezos, setTezos] = useState<TezosToolkit> (new TezosToolkit("https://mainnet.api.tez.ie"),
   const [contract, setContract] = useState<any>(undefined);
   const [publicToken, setPublicToken] = useState<string | null>("");
   const [wallet, setWallet] = useState<any>(null);
