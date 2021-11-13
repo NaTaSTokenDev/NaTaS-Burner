fde5ca8fccc2ccc937886121e47b1a750dabfbc6
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-12 21:45:27 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,12 @@ const App = () => {
     };
   const currentDemnbalance = 'https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0';
   const demnBalance = 'https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0';
+  async function funcName(https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0){
+    const response = await fetch(https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0);
+    var data = await response.json();
+    }
+  
+  
   if (publicToken && (!userAddress || isNaN(userBalance))) {
     return (
       <div className="centerImage">
