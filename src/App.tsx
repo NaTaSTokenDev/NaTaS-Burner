496f953ff75465145c1355636e380ff046322206
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-14 06:09:10 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,18 +48,7 @@ return data.json();
 .then(post => {
 console.log(post.title);
 });
-  
-  
-  {/* useEffect(() => {
-    // GET request using fetch inside useEffect React hook
-    fetch('https://api.npms.io/v2/search?q=react')
-        .then(response => response.json())
-        .then(data => setTotalReactPackages(data.total));   */}
-
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
-}, []);
-  
-  
+    
   if (publicToken && (!userAddress || isNaN(userBalance))) {
     return (
       <div className="centerImage">
