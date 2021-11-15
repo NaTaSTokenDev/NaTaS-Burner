8177ba549f2ae76c8953a4402514eb82cfad0a44
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-14 20:03:49 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
   const [activeTab, setActiveTab] = useState<string>("transfer");
   const contractAddress: string = "KT1GBgCd5dk7v4TSzWvtk1X64TxMyG4r7eRX";  
  // const demnBalance = () => {
-//     return fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
+ //     return fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
  //      .then((response) => response.json())
  //      .then((data) => console.log(data));}
   const generateQrCode = (): { __html: string } => {
@@ -41,7 +41,7 @@ const App = () => {
     return { __html: qr.createImgTag(4) };
 
 
- const demnBalance = () => {fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
+ const demnBalance = () => {return fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
   .then(response => response.json())
   .then(data => console.log(data));};
     
