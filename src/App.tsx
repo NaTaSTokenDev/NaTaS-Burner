c6344bf9be30a8ebdc10a0bf3be68998bc71bba2
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-14 20:10:02 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,10 +40,10 @@ const App = () => {
     qr.make();
     return { __html: qr.createImgTag(4) };
 
-
- const demnBalance = () => {return fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
-  .then(response => response.json())
-  .then(data => console.log(data));};
+  const demnBalance: string = "KT1GBgCd5dk7v4TSzWvtk1X64TxMyG4r7eRX";  
+ // const demnBalance = () => {return fetch("https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0")
+ // .then(response => response.json())
+ // .then(data => console.log(data));};
     
     
     };
