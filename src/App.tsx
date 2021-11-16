2a2d377c3c99e6f95e6b273cfd67194a3c4db638
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-16 18:57:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,29 @@ const App = () => {
             totalReactPackages: null
         };
     }
+    
+     componentDidMount() {
+        // Simple GET request using fetch
+        fetch('https://api.npms.io/v2/search?q=react')
+            .then(response => response.json())
+            .then(data => this.setState({ totalReactPackages: data.total }));
+    }
+    
   
+    render() {
+        const { totalReactPackages } = this.state;
+        return (
+            <div className="card text-center m-3">
+                <h5 className="card-header">Simple GET Request</h5>
+                <div className="card-body">
+                    Total react packages: {totalReactPackages}
+                </div>
+            </div>
+        );
+    }
+}
+    
+    
  const bob = fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.json())
   .then(data => console.log(data));
