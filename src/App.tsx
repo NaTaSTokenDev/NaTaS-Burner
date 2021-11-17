891f1ed2a52161254511cb0c65899ffbe312803c
NaTaSTokenDev <92642758+NaTaSTokenDev@users.noreply.github.com>
2021-11-16 19:37:56 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
     return { __html: qr.createImgTag(4) };
     };
  // const currentDemnbalance = 'https://api.better-call.dev/v1/contract/mainnet/KT1GaEvbD4zA3pHs7mv3grpuqR1KGtjXAEDe/tokens/holders?token_id=0';
-
+const rad = (
      componentDidMount(); {
         // Simple GET request using fetch
         fetch('https://api.npms.io/v2/search?q=react')
@@ -58,7 +58,7 @@ const App = () => {
         );
     }
 }
-    
+);
     
  const bob = fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.json())
